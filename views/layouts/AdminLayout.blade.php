28f14a59adc2f0747f5e79d74a3f4aaa98848eec
VietAnh <mgbaiser@gmail.com>
2026-03-13 00:26:52 +0700

    feat: Add menu listing page with category filtering and search functionality.

--- views/layouts/AdminLayout.blade.php
+++ views/layouts/AdminLayout.blade.php
@@ -1,65 +1,65 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <!-- ===============================================-->
-    <!--    Document Title-->
-    <!-- ===============================================-->
-    <title>Foodwagon | Responsive, Ecommerce &amp; Business Templatee</title>
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Foodwagon | Responsive, Ecommerce &amp; Business Templatee</title>
 
 
-    <!-- ===============================================-->
-    <!--    Favicons-->
-    <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicons/favicon.ico') }}">
-    <link rel="manifest" href="{{ asset('img/favicons/manifest.json') }}">
-    <meta name="msapplication-TileImage" content="{{ asset('img/favicons/mstile-150x150.png') }}">
-    <meta name="theme-color" content="#ffffff">
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicons/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicons/favicon-16x16.png') }}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicons/favicon.ico') }}">
+  <link rel="manifest" href="{{ asset('img/favicons/manifest.json') }}">
+  <meta name="msapplication-TileImage" content="{{ asset('img/favicons/mstile-150x150.png') }}">
+  <meta name="theme-color" content="#ffffff">
 
 
-    <!-- ===============================================-->
-    <!--    Stylesheets-->
-    <!-- ===============================================-->
-    <link href="{{ asset('assets/css/theme.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/menu-filter.css') }}" rel="stylesheet" />
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="{{ asset('assets/css/theme.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/menu-filter.css') }}" rel="stylesheet" />
 
-  </head>
-
-
-  <body>
-
-    <!-- ===============================================-->
-    <!--    Main Content-->
-    <!-- ===============================================-->
-    @include('layouts.blocks.header')
-    @yield('content')
-    <!-- ===============================================-->
-    <!--    End of Main Content-->
-    <!-- ===============================================-->
+</head>
 
 
+<body>
+
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  @include('layouts.blocks.header')
+  @yield('content')
+  <!-- ===============================================-->
+  <!--    End of Main Content-->
+  <!-- ===============================================-->
 
 
-    <!-- ===============================================-->
-    <!--    JavaScripts-->
-    <!-- ===============================================-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets/js/vendors/@popperjs/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendors/bootstrap/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendors/is/is.min.js') }}"></script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-    <script src="{{ asset('assets/js/vendors/fontawesome/all.min.js') }}"></script>
-    <script src="{{ asset('assets/js/theme.js') }}"></script>
 
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
-  </body>
+
+  <!-- ===============================================-->
+  <!--    JavaScripts-->
+  <!-- ===============================================-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('assets/js/vendors/@popperjs/popper.min.js') }}"></script>
+  <script src="{{ asset('assets/js/vendors/bootstrap/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('assets/js/vendors/is/is.min.js') }}"></script>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+  <script src="{{ asset('assets/js/vendors/fontawesome/all.min.js') }}"></script>
+  <script src="{{ asset('assets/js/theme.js') }}"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
+  @yield('scripts')
+</body>
 
 </html>

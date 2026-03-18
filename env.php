7625bb5f0e9fda34b202d64ce54646a31e3c3f06
Huyen318006 <ngochuyen318006@gmail.com>
2026-03-18 19:51:48 +0700

    Update environment variables and enhance routing in templates

--- env.php
+++ env.php
@@ -2,13 +2,13 @@
 // File chứa các biến muôi trường trong hệ thống
 // Khai báo các biến dưới dạng HẰNG SỐ để ko phải sử dụng $GLOBALS
 
-define('APP_URL', 'http://localhost/DuanAgile/'); // Đường dẫn dạy dự án
+define('APP_URL', 'http://quanliduanvoiagile.test/'); // Đường dẫn dạy dự án
 // Lưu ý cấp thư mục sẽ phải là htdocs hoặc www tương tự http://localhost/
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'quanliduanagile'); // Tên database //
+define('DB_NAME', 'quanliduanvoiagile'); // Tên database //
 
 define('PATH_ROOT', __DIR__ . '/');
